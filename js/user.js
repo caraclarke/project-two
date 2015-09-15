ecc285d0b4e3c0c3b90706dbcab8f18980492e9d
caraclarke <cara.clarke8@gmail.com>
2015-09-15 14:26:25 -0400

    Changing from login page to modals
    Switching project from using login/register pages to using modals.

--- js/user.js
+++ js/user.js
@@ -2,9 +2,17 @@ $(function() {
   'use strict';
   var sa = 'https://shielded-ocean-1335.herokuapp.com';
 
+  $("#show-login-modal").on('click', function() {
+    $("#login-modal").modal();
+  });
+
+  $("#show-register-modal").on('click', function() {
+    $("#register-modal").modal();
+  });
+
 // User
 
-$('#register').on('click', function(e) {
+$('#register-button').on('click', function(e) {
   var reader = new FileReader();
   reader.onload = function(event) {
     var picture = event.target.result;
@@ -37,7 +45,7 @@ $('#register').on('click', function(e) {
     reader.readAsDataURL($('#picture')[0].files[0]);
   }); // end register
 
-$('#login').on('click', function(e) {
+$('#login-button').on('click', function(e) {
   e.preventDefault();
   $.ajax({
     url: sa + '/login',
@@ -52,10 +60,10 @@ $('#login').on('click', function(e) {
     dataType: 'json',
     method: 'POST'
   }).done(function(data, textStatus, jqxhr) {
+    $("#login-modal").modal('hide');
     simpleStorage.set('token', data.token);
     simpleStorage.set('userId', data.user_id);
     simpleStorage.set('profileId', data.profile_id);
-    window.location.href = '/index.html';
   }).fail(function(jqxhr, textStatus, errorThrown) {
     alert("Email or Password incorrect, please try again.");
   });
