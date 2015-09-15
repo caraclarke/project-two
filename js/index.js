789029917b14cec1f319fc2008654c4c2a2d0563
caraclarke <cara.clarke8@gmail.com>
2015-09-15 13:57:21 -0400

    Syntax fixes in index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(function() {
   'use strict';
   var sa = 'https://shielded-ocean-1335.herokuapp.com';
 
-  $('#contact-submit').on('click', function(event){
+  $('#contact-submit').on('click', function(event) {
     alert("Thank you for contacting us! We'll be in touch shortly.");
     window.location.href = '/index.html';
   });
