413090d756bf5fc664c7b4bf6ec4d87a7656dd4e
caraclarke <cara.clarke8@gmail.com>
2015-09-15 14:02:54 -0400

    Syntax fixes in workshop.js

--- js/workshop.js
+++ js/workshop.js
@@ -4,7 +4,7 @@ $(function() {
 
 // Workshops
 
-$("#workshop-create").on('click', function(e){
+$("#workshop-create").on('click', function(e) {
     $.ajax({
       url: sa + '/workshops',
       method: 'POST',
@@ -20,46 +20,44 @@ $("#workshop-create").on('click', function(e){
           repeats: $('#workshop-repeats').val()
         }
       }
-    }).done(function(data){
+    }).done(function(data) {
       console.log("Created workshop!");
       $('#result').val(JSON.stringify(data)); // update to display in handlebars
-    }).fail(function(data){
+    }).fail(function(data) {
       console.error(data);
     });
-  }
   projectReader.readAsDataURL($('#project-picture')[0].files[0]);
 }); // end workshop create
 
-
-$("#workshop-show").on('click', function(event){
+$("#workshop-show").on('click', function(event) {
   $.ajax({ // change this button
     url: sa + "/workshops/" + $("#workshop-id").val(),
-  }).done(function(workshop){
+  }).done(function(workshop) {
     var workshopShowTemplate = Handlebars.compile($('#workshop-show-template').html());
     var newHTML = workshopShowTemplate;
     $("#show-workshop").html(newHTML);
     console.log(JSON.stringify(data));
-  }).fail(function(data){
+  }).fail(function(data) {
     console.error(data);
   });
 }); // end workshop show
 
-$("#workshop-index").on('click', function(event){
+$("#workshop-index").on('click', function(event) {
   $.ajax({
     url: sa + "/workshops",
-  }).done(function(data){
+  }).done(function(data) {
     var workshopIndexTemplate = Handlebars.compile($('#workshop-index-template').html());
     var newHTML = workshopIndexTemplate;
     $("#index-workshop").html(newHTML);
 
     console.log(JSON.stringify(data));
   });
-}).fail(function(data){
+}).fail(function(data) {
   console.error(data);
 });
 }); // end workshop index
 
-$("#workshop-update").on('click', function(){
+$("#workshop-update").on('click', function() {
   $.ajax({
     url: sa + '/workshops/' + $("#workshop-id").val(),
     method: 'PATCH',
@@ -75,27 +73,27 @@ $("#workshop-update").on('click', function(){
         repeats: $('#workshop-repeats').val()
       }
     }
-  }).done(function(data, textStatus, jqxhr){
+  }).done(function(data, textStatus, jqxhr) {
     var workshopShowTemplate = Handlebars.compile($('#workshop-show-template').html());
     var newHTML = workshopShowTemplate;
     $("#show-workshop").html(newHTML);
 
     console.log(JSON.stringify(data));
- }).fail(function(jqxhr, textStatus, errorThrown){
+ }).fail(function(jqxhr, textStatus, errorThrown) {
   console.error(errorThrown);
 });
 }); // end workshop update
 
-$("#workshop-destroy").on('click', function(){
+$("#workshop-destroy").on('click', function() {
   $.ajax({
     url: sa + '/workshops/' + $("#workshop-id").val(),
     method: 'DELETE',
     headers: {
       Authorization: 'Token token=' + simpleStorage.get('token')
     }, // authenticate creator not just logged in
-  }).done(function(data){
+  }).done(function(data) {
     console.log("Deleted workshop!");
-  }).fail(function(data){
+  }).fail(function(data) {
     console.error(data);
   });
 }); // end workshop destroy
@@ -104,7 +102,7 @@ $("#workshop-destroy").on('click', function(){
 
 // Attendances
 
-$("#attendance-create").on('click', function(e){
+$("#attendance-create").on('click', function(e) {
     $.ajax({
       url: sa + '/attendances',
       method: 'POST',
@@ -118,38 +116,36 @@ $("#attendance-create").on('click', function(e){
           user_name: $('attendance-name').val()
         }
       }
-    }).done(function(data){
+    }).done(function(data) {
       console.log("Created attendance!");
       var attendanceIndexTemplate = Handlebars.compile($('#attendance-index-template').html());
       var newHTML = attendanceIndexTemplate;
       $("#index-attendance").html(newHTML);
 
       console.log(JSON.stringify(data));
-    }).fail(function(data){
+    }).fail(function(data) {
       console.error(data);
     });
-  }
 }); // end attendance create
 
-$("#attendance-index").on('click', function(event){
+$("#attendance-index").on('click', function(event) {
   $.ajax({
     url: sa + "/attendances",
     headers: {
       Authorization: 'Token token=' + simpleStorage.get('token')
     }
-  }).done(function(data){
+  }).done(function(data) {
     var attendanceIndexTemplate = Handlebars.compile($('#attendance-index-template').html());
     var newHTML = attendanceIndexTemplate;
     $("#index-attendance").html(newHTML);
 
     console.log(JSON.stringify(data));
-  });
-}).fail(function(data){
-  console.error(data);
-});
+  }).fail(function(data) {
+    console.error(data);
+  }); // end fail
 }); // end attendance index
 
-$("#attendance-update").on('click', function(){
+$("#attendance-update").on('click', function() {
   $.ajax({
     url: sa + '/attendances/' + $("#attendace-id").val(),
     method: 'PATCH',
@@ -163,31 +159,29 @@ $("#attendance-update").on('click', function(){
         user_name: $('attendance-name').val()
       }
     }
-  }).done(function(data, textStatus, jqxhr){
+  }).done(function(data, textStatus, jqxhr) {
    var attendanceIndexTemplate = Handlebars.compile($('#attendance-index-template').html());
     var newHTML = attendanceIndexTemplate;
     $("#index-attendance").html(newHTML);
 
     console.log(JSON.stringify(data));
- }).fail(function(jqxhr, textStatus, errorThrown){
+ }).fail(function(jqxhr, textStatus, errorThrown) {
   console.error(errorThrown);
 });
 }); // end attendance update
 
-$("#attendance-destroy").on('click', function(){
+$("#attendance-destroy").on('click', function() {
   $.ajax({
     url: sa + '/attendances/' + $("#attendance-id").val(),
     method: 'DELETE',
     headers: {
       Authorization: 'Token token=' + simpleStorage.get('token')
     }, // authenticate creator not just logged in
-  }).done(function(data){
+  }).done(function(data) {
     console.log("Deleted attendance!");
-  }).fail(function(data){
+  }).fail(function(data) {
     console.error(data);
-  });
 }); // end attendance destroy
-
 // end Attendances
 
 }); // end function
