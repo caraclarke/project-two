b1efb889fafa985a267bf0a99c9c28a63a96a01e
caraclarke <cara.clarke8@gmail.com>
2015-09-17 13:50:47 -0400

    Updated handlebars file, updated navbar in index

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
+var sa = 'https://shielded-ocean-1335.herokuapp.com';
+var test = 'http://localhost:3000';
+
+var navbarTemplate = function (data) {
+	var navbarShowTemplate = Handlebars.compile($("#navbar-template").html());
+	$("#navbar").html(navbarShowTemplate(data));
+};
+
+var showNavbar = function() {
+  $.ajax({
+    url: './js/navbar.hbs',
+		// cache: true
+  }).done(function(data) {
+    // var profileShowTemplate = Handlebars.compile($("#profile-show-template").html());
+    // $("#show-profile").html(profileShowTemplate(data.profile));
+    navbarTemplate(data);
+  }).fail(function(data) {
+    console.error(data);
+    // window.location.href = '/login_page.html';
+  });
+};
+
 $(function() {
   'use strict';
-  var sa = 'https://shielded-ocean-1335.herokuapp.com';
-	var test = 'http://localhost:3000';
 
-  $('#contact-submit').on('click', function(event) {
-    alert("Thank you for contacting us! We'll be in touch shortly.");
-    window.location.href = '/index.html';
-  });
+	showNavbar();
 
 }); // end function
