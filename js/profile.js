3f40addf5272d90349b04ed4a6e5915703e5d7ce
caraclarke <cara.clarke8@gmail.com>
2015-09-24 12:05:17 -0400

    Trying to autofill update form

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
 var sa = 'https://shielded-ocean-1335.herokuapp.com';
-// var test = 'http://localhost:3000';
+var test = 'http://localhost:3000';
 
 var showProfileInformationTemplate = function (data) {
   var profileShowTemplate = Handlebars.compile($("#profile-show-template").html());
@@ -14,10 +14,24 @@ var showProfile = function() {
     },
   }).done(function(data) {
     showProfileInformationTemplate(data);
+    console.log(data);
   }).fail(function(data) {
     console.error(data);
   });
 };
+
+// not currently using
+var populateUpdateForm = function (data) {
+  if (data) {
+    //$('#profile-update-template').data('id', data.id);
+    $('#given_name').val(data.given_name);
+    $('#surname').val(data.surname);
+    $('#location').val(data.location);
+    $('#gender').val(data.gender);
+    $('#about_me').val(data.about_me);
+    // $('').append('<img src="' + data.profile_picture + '" alt="Profile Picture">'  );
+  };
+}
 
 $(function() {
   'use strict';
@@ -39,11 +53,15 @@ $(function() {
     $.ajax({
       url: sa + '/profiles/' + simpleStorage.get('profileId')
     }).done(function(data) {
+      // populateUpdateForm(data);
       showEditProfileForm(data);
     });
   });
 
-  $("#update-profile").on('click', '#profile-submit-update' ,function(event) {
+  // $('#form').populate({first_name:'Dave',last_name:'Stewart'})
+  // {name:value, array:[value, value, value], object:{name:value}}
+
+  $("#update-profile").on('click', '#profile-submit-update', function(event) {
     $.ajax({
       url: sa + '/profiles/' + simpleStorage.get('profileId'),
       method: 'PATCH',
@@ -64,7 +82,7 @@ $(function() {
    }).done(function(data, textStatus, jqxhr) {
      var profileShowTemplate = Handlebars.compile($("#profile-show-template").html());
       $("#show-profile").html(profileShowTemplate(data.profile));
-     hideEditProfileForm();
+      hideEditProfileForm();
    }).fail(function(jqxhr, textStatus, errorThrown) {
      console.error(errorThrown);
    });
