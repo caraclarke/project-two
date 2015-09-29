b2075f31ac257d2aacea22845b3774607e306f10
caraclarke <cara.clarke8@gmail.com>
2015-09-29 17:45:46 -0400

    Removed console.logs

--- js/index.js
+++ js/index.js
@@ -3,15 +3,11 @@ var test = 'http://localhost:3000';
 
 var loggedIn = function() {
   if (simpleStorage.get('token')) {
-    console.log(simpleStorage.get('token'));
-    console.log("hey friends");
     $('#show-login-modal').addClass('hide');
     $('#show-register-modal').addClass('hide');
     $('#nav-logout').removeClass('hide');
     $('#nav-profile').removeClass('hide');
   } else {
-    console.log("you yo yo");
-    console.log(simpleStorage.get('token'));
     $('#nav-logout').addClass('hide');
     $('#show-login-modal').removeClass('hide');
     $('#show-register-modal').removeClass('hide');
